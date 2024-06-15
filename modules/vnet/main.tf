f913b354ee498e86d386b9859730bfff61f9bc07
fdlm <fmirouze@hotmail.com>
2024-06-15 16:38:31 +0200

    -

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -10,27 +10,27 @@ resource "azurerm_virtual_network" "vnet" {
     content {
       name           = subnet.value.subnet_name
       address_prefix = subnet.value.subnet_address_space
-      security_group = subnet.value.subnet_name != "GatewaySubnet" ? azurerm_network_security_group.nsg_group.id : null
+      # security_group = subnet.value.subnet_name != "GatewaySubnet" ? azurerm_network_security_group.nsg_group.id : null
     }
   }
 }
 
-resource "azurerm_network_security_rule" "nsg" {
-  name                        = "ICMP-Allow"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Icmp"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_group.name
-}
+# resource "azurerm_network_security_rule" "nsg" {
+#   name                        = "ICMP-Allow"
+#   priority                    = 100
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Icmp"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = var.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.nsg_group.name
+# }
 
-resource "azurerm_network_security_group" "nsg_group" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
+# resource "azurerm_network_security_group" "nsg_group" {
+#   name                = "acceptanceTestSecurityGroup1"
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+# }
